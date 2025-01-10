a8eb22b6dabe0b4d7e485fab9fb272fb72c67a1c
Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>
2025-01-11 02:57:36 +0530

    misc: remove bin folder

--- bin/schedule_parser.dart
+++
@@ -1,9 +0,0 @@
-import 'package:schedule_parser/schedule_parser.dart';
-
-void main() async {
-  final schedule =
-      await ScheduleParser.fromUrl('https://fosdem.org/2025/schedule/xml');
-  for (var track in schedule.tracks) {
-    print('Track: ${track.name} | Online QA? ${track.onlineQa}');
-  }
-}
