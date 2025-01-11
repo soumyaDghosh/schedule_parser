eaafc39cc45493ada945c7753b0ce3e8bca204c0
Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>
2025-01-11 10:02:59 +0530

    misc: add license and license headers in each file

--- lib/src/event.dart
+++ lib/src/event.dart
@@ -1,3 +1,12 @@
+// Copyright (C) 2024 Soumyadeep Ghosh
+// This file is part of schedule_parser.
+//
+// shedule_parser is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation version 3 of the License.
+//
+// schedule_parser is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with schedule_parser. If not, see <https://www.gnu.org/licenses/>.
+
 import 'package:xml/xml.dart';
 
 import 'person.dart';
